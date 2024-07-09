cb3cea1086ae93844a4fec30b7d9d74e1b412eb9
ALLAN MASIGA <zionalehebrew@gmail.com>
2024-07-09 08:23:49 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import LoadMoreData from './load-more-data';
+import './App.css'
+
+
+function App() {
+  
+
+  return <LoadMoreData/>;
+    
+}
+
+export default App
